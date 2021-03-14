3450c581985babba0fe3f0ca587d1bb09a306b52
Efrain Coello <carlosefrain17@hotmail.com>
2021-03-14 14:32:37 -0500

    first commit

---
+++ lpp/token.py
@@ -0,0 +1,39 @@
+from enum import (
+    auto,
+    Enum,
+    unique,
+)
+from typing import NamedTuple, Dict
+
+
+@unique
+class TokenType(Enum):
+    ASSIGN = auto()
+    COMMA = auto()
+    EOF = auto()
+    FUNCTION = auto()
+    IDENT = auto()
+    ILLEGAL = auto()
+    INT = auto()
+    LBRACE = auto()
+    LET = auto()
+    LPAREN = auto()
+    PLUS = auto()
+    RBRACE = auto()
+    RPAREN = auto()
+    SEMICOLON = auto()
+
+
+class Token(NamedTuple):
+    token_type: TokenType
+    literal: str
+
+    def __str__(self) -> str:
+        return f'Type: {self.token_type}, Literal: {self.literal}'
+
+
+def lookup_token_type(literal: str) -> TokenType:
+    keywords: Dict[str, TokenType] = {
+        'variable': TokenType.LET
+    }
+    return keywords.get(literal, TokenType.IDENT)
